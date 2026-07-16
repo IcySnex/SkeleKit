310e37d43529ab934c4725237c3823cf2c49b7a1
IcySnex <81762750+IcySnex@users.noreply.github.com>
2026-07-16 11:47:57 +0200

    fix(generators): emit pages only

--- Samples/BareUI.Gallery/Program.cs
+++ Samples/BareUI.Gallery/Program.cs
@@ -12,6 +12,35 @@ BareApplication.CreateBuilder()
 		services.AddSingleton<IDemoCatalog, DemoCatalog>();
 		services.AddSingleton<IMovieService, MovieService>();
 		services.AddSingleton<PlayerBarViewModel>();
+
+		services.AddTransient<MenuViewModel>();
+		services.AddTransient<MovieInfoViewModel>();
+		services.AddTransient<BindingViewModel>();
+		services.AddTransient<StylingDemoViewModel>();
+		services.AddTransient<ChromeDemoViewModel>();
+		services.AddTransient<AccessoryDemoViewModel>();
+		services.AddTransient<AnimationDemoViewModel>();
+		services.AddTransient<ButtonDemoViewModel>();
+		services.AddTransient<TextFieldDemoViewModel>();
+		services.AddTransient<TextEditorDemoViewModel>();
+		services.AddTransient<SwitchDemoViewModel>();
+		services.AddTransient<SegmentedDemoViewModel>();
+		services.AddTransient<DatePickerDemoViewModel>();
+		services.AddTransient<TintDemoViewModel>();
+		services.AddTransient<PageControlDemoViewModel>();
+		services.AddTransient<SliderDemoViewModel>();
+		services.AddTransient<StepperDemoViewModel>();
+		services.AddTransient<ProgressBarDemoViewModel>();
+		services.AddTransient<ActivityIndicatorDemoViewModel>();
+		services.AddTransient<DividerDemoViewModel>();
+		services.AddTransient<PickerDemoViewModel>();
+		services.AddTransient<ImageDemoViewModel>();
+		services.AddTransient<NativeViewDemoViewModel>();
+		services.AddTransient<KeyboardDemoViewModel>();
+		services.AddTransient<GridDemoViewModel>();
+		services.AddTransient<ListDemoViewModel>();
+		services.AddTransient<CarouselDemoViewModel>();
+		services.AddTransient<LiveListDemoViewModel>();
 	})
 	.UseTheme(theme => theme
 		.Style(new Style<Label>(label => label.TextColor = Colors.Label))
