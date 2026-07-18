f94ca396050955060fa49bb7004110cabc81c424
IcySnex <81762750+IcySnex@users.noreply.github.com>
2026-07-18 12:17:03 +0200

    demo: share each item kind separately

--- Samples/BareUI.Gallery/Views/Demos/WebViewDemo.cs
+++ Samples/BareUI.Gallery/Views/Demos/WebViewDemo.cs
@@ -26,6 +26,28 @@ public class WebViewDemo : ContentView<WebViewDemoViewModel>
 		ToolbarItems.Add(new() { Icon = "chevron.backward", Command = Command.From(web.GoBack) });
 		ToolbarItems.Add(new() { Icon = "arrow.clockwise", Command = Command.From(web.Reload) });
 		ToolbarItems.Add(new() { Icon = "safari", Command = Command.From(() => _ = Navigator.OpenUrlAsync(viewModel.Url)) });
-		ToolbarItems.Add(new() { Icon = "square.and.arrow.up", Command = Command.From(() => _ = Sharer.ShareAsync("Check this page", new Uri(viewModel.Url), ImageSource.Symbol("globe"))) });
+		ToolbarItems.Add(new() { Icon = "square.and.arrow.up", Command = Command.From(() => _ = Share()) });
+
+		// each choice shares a different item kind, so the sheet's actions visibly change
+		async Task Share()
+		{
+			Uri url = new(viewModel.Url);
+
+			switch (await Navigator.ActionSheetAsync("Share", "Cancel", "Text", "Link", "Image", "Text + link + image"))
+			{
+				case "Text":
+					await Sharer.ShareAsync("Check out this page");
+					break;
+				case "Link":
+					await Sharer.ShareAsync(url);
+					break;
+				case "Image":
+					await Sharer.ShareAsync(ImageSource.Symbol("globe"));
+					break;
+				case "Text + link + image":
+					await Sharer.ShareAsync("Check out this page", url, ImageSource.Symbol("globe"));
+					break;
+			}
+		}
 	}
 }
